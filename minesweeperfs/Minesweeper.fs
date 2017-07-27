aa916ee86ec86aff9b43d2ad9c52a76dae4ccabb
Kenneth Crawford <kmc059000@gmail.com>
2017-07-26 20:47:52 -0500

    generating game and printing game

---
+++ minesweeperfs/Minesweeper.fs
@@ -0,0 +1,60 @@
+﻿module Minesweeper
+
+type GameState = Start | Playing | Win | Dead
+
+type CellState = Hidden | Exposed | Flagged
+type Cell = {
+    State: CellState;
+    Index: int;
+    IsMine: bool;
+};
+
+type Game = {
+    Cells: Cell[];
+    State: GameState;
+    Width: int;
+    Height: int;
+    MineLocations: Set<int>;
+    SecondaryMineLocations: Set<int>;
+};
+
+let getSurroundingCount (game:Game) (cell:Cell) =
+    //todo find number of cells around this cell which has a mine
+    5
+
+let createGame (width:int) (height:int) (mineCount:int) (rand:System.Random) =
+    let createCell (primaryMineLocations:Set<int>) index = {
+        State = Hidden;
+        Index = index;
+        IsMine = primaryMineLocations.Contains index
+    }
+
+    let maxIndex = width * height
+    
+    let allMineLocations =
+        Seq.initInfinite (fun _ -> rand.Next(maxIndex))
+        |> Seq.distinct
+        |> Seq.take (mineCount + 1)
+        |> List.ofSeq
+
+    let primaryMineLocations = allMineLocations |> Seq.take (mineCount) |> Set.ofSeq
+    let secondaryMineLocation = allMineLocations |> Seq.skip 1 |> Seq.take (mineCount) |> Set.ofSeq
+    
+    let cells = 
+        [1..(width * height)] 
+        |> Seq.map (createCell primaryMineLocations)
+        |> Seq.toArray
+    {
+        Cells = cells;
+        State = GameState.Start;
+        Width = width;
+        Height = height;
+        MineLocations = primaryMineLocations;
+        SecondaryMineLocations = secondaryMineLocation
+    }
+
+let createEasyGame<'a> = createGame 10 10 10
+let createMediumGame<'a> = createGame 20 20 80
+let createHardGame<'a> = createGame 30 30 400
+
+
---
+++ minesweeperfs/Program.fs
@@ -0,0 +1,35 @@
+﻿// Learn more about F# at http://fsharp.org
+// See the 'F# Tutorial' project for more help.
+open Minesweeper
+
+let getCellChar (game:Game) (cell:Cell) =
+    match cell.State with
+    | Hidden -> "·"
+    | Exposed -> 
+        match getSurroundingCount game cell with
+        | 0 -> " "
+        | i -> i.ToString()
+    | Flagged -> "?"
+
+
+
+let getRowText (game:Game) (row:Cell[]) = 
+    row |> Array.map (getCellChar game) |> String.concat ""
+
+let printGame (game:Game) =
+    let rows = 
+        game.Cells
+        |> Array.chunkBySize game.Width
+        |> Array.map (getRowText game)
+    printfn "Game:"
+    for r in rows do printfn "%s" r
+    printfn ""
+    printfn ""
+
+let mutable game = createEasyGame (new System.Random())
+
+[<EntryPoint>]
+let main argv = 
+    printGame game
+    let e = System.Console.ReadLine()
+    0 // return an integer exit code
