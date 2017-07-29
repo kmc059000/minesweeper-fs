bd79eda4673b5b0b9a23e3d1542725cacb8c9788
Kenneth Crawford <kmc059000@gmail.com>
2017-07-29 15:59:32 -0500

    made solvers run in parallel for faster testing

--- minesweeper.solvers/Program.fs
+++ minesweeper.solvers/Program.fs
@@ -5,18 +5,22 @@ open RandomSolver
 let createGame i = 
     let rand = new System.Random()
     let randoms = ([0..1000] |> List.map (fun _ -> rand.Next()) |> Array.ofList)
-    createEasyGame randoms 
+    (createEasyGame randoms, i)
 
-let games = [0..10000] |> List.map createGame |> Array.ofList
+let games = [0..10000] |> Seq.map createGame
 
-let testSolver (solver:Game->Solution) game = async {
+let testSolver (solver:Game->Solution) game = 
     let solved = solver game
-    return solved.SolutionState
+    solved.SolutionState
+
+let testSolverAsync (solver:Game->Solution) (game,i) = async {
+    if i % 1000 = 0 then printf "."
+    return testSolver solver game
 }
 
 let rec runSolverTests (solver:Game->Solution) numTests previousResults =
     games
-    |> Seq.map (testSolver solver)
+    |> Seq.map (testSolverAsync solver)
     |> Async.Parallel
     |> Async.RunSynchronously
     |> Seq.fold (fun acc elm -> 
@@ -26,19 +30,6 @@ let rec runSolverTests (solver:Game->Solution) numTests previousResults =
             | Dead -> (successes, total + 1)
             | _ -> failwith "Unexpected solution state") 
         (0,0)
-    //match numTests with
-    //| 0 -> previousResults
-    //| _ -> 
-    //    let rand = new System.Random()
-    //    let randoms = ([0..1000] |> List.map (fun _ -> rand.Next()) |> Array.ofList)
-    //    let runResult = createEasyGame randoms |> testSolver solver
-    //    let successes, total = previousResults
-    //    let newResults =
-    //        match runResult with
-    //        | Win -> (successes + 1, total + 1)
-    //        | Dead -> (successes, total + 1)
-    //        | _ -> failwith "Unexpected solution state"
-    //    runSolverTests solver (numTests - 1) newResults
 
 let testRandomSolver = runSolverTests randomSolver 10000 (0,0)
 
