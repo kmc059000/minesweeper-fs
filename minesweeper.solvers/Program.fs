4bf16c7cad67726068a5ca4201c35212bfd676bb
Kenneth Crawford <kmc059000@gmail.com>
2017-07-30 22:45:25 -0500

    printing results

--- minesweeper.solvers/Program.fs
+++ minesweeper.solvers/Program.fs
@@ -35,12 +35,13 @@ let rec runSolverTests (solver:Game->Solution) previousResults =
 
 
 
-//let testRandomSolver = runSolverTests randomSolver (0,0)
+let testRandomSolver = runSolverTests randomSolver (0,0)
 let testProbabilitySolver = runSolverTests probabilitySolver (0,0)
 
 [<EntryPoint>]
 let main argv = 
-    //printf "%s" (testRandomSolver.ToString())
-    printf "%s" (testProbabilitySolver.ToString())
+    printfn "" 
+    printfn "Random: %s %f%%" (testRandomSolver.ToString()) ((float <| fst testRandomSolver) / (float <| snd testRandomSolver))
+    printfn "Probability Based: %s %f%%" (testProbabilitySolver.ToString()) ((float <| fst testProbabilitySolver) / (float <| snd testProbabilitySolver))
     let r = System.Console.ReadLine()
     0
