ad7119728dac00453dd3b435afaa1be21403a00d
Kenneth Crawford <kmc059000@gmail.com>
2017-08-05 17:19:50 -0500

    cleanup

--- minesweeperfs/Program.fs
+++ minesweeperfs/Program.fs
@@ -14,19 +14,19 @@ open MinesweeperUI
 //debug <- true
 
 let rec requestGameSize _ = 
-    System.Console.WriteLine("What difficulty would you like to play? (type the number)")
-    System.Console.WriteLine("1 - Easy")
-    System.Console.WriteLine("2 - Medium")
-    System.Console.WriteLine("3 - Hard")
-    System.Console.WriteLine("")
-    let difficulty = System.Console.ReadLine()
+    Console.WriteLine("What difficulty would you like to play? (type the number)")
+    Console.WriteLine("1 - Easy")
+    Console.WriteLine("2 - Medium")
+    Console.WriteLine("3 - Hard")
+    Console.WriteLine("")
+    let difficulty = Console.ReadLine()
     match difficulty with
     | "1" -> GameFactory.createEasyGame
     | "2" -> GameFactory.createMediumGame
     | "3" -> GameFactory.createHardGame
     | _ -> 
-        System.Console.Clear()
-        System.Console.WriteLine("Unknown difficulty: " + difficulty)
+        Console.Clear()
+        Console.WriteLine("Unknown difficulty: " + difficulty)
         requestGameSize ()
 
 
@@ -40,32 +40,32 @@ let getAction (cursor:Coordinates.Coordinate) key =
     let x = cursor.X
     let y = cursor.Y
     match key with
-    | System.ConsoleKey.LeftArrow -> moveLeft
-    | System.ConsoleKey.RightArrow -> moveRight
-    | System.ConsoleKey.UpArrow -> moveUp
-    | System.ConsoleKey.DownArrow -> moveDown
-    | System.ConsoleKey.Q -> quit
-    | System.ConsoleKey.Spacebar -> sweep x y
-    | System.ConsoleKey.F -> flag x y
+    | ConsoleKey.LeftArrow -> moveLeft
+    | ConsoleKey.RightArrow -> moveRight
+    | ConsoleKey.UpArrow -> moveUp
+    | ConsoleKey.DownArrow -> moveDown
+    | ConsoleKey.Q -> quit
+    | ConsoleKey.Spacebar -> sweep x y
+    | ConsoleKey.F -> flag x y
     | _ -> id
 
 //unpure method
 let gameloop seed =
     let gameFactory = requestGameSize ()
-    System.Console.Clear()
+    Console.Clear()
     let mutable console = ConsoleText.emptyUI
 
     let mutable game = gameFactory seed
 
     while game.State <> GameState.Exit && game.State <> GameState.Quit do
         console <- printGame console game
-        let key = System.Console.ReadKey().Key
+        let key = Console.ReadKey().Key
         let handleAction = getAction game.CursorPosition key
         game <- handleAction game
     
 
 [<EntryPoint>]
 let main argv =
-    let rand = new System.Random()
+    let rand = new Random()
     gameloop (rand.Next())
     0
